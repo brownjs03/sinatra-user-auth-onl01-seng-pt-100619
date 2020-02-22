07ca17edb1410aa4e8a70c5cae181000fcc68ba5
Jeffrey Brown <brownjs03@gmail.com>
2020-02-22 01:30:03 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    @user = User.find_by(params[:email], params[:password])
+    @user = User.find_by(email: params[:email], password: params[:password])
     if @user
       session[:user_id] = @user.id
       redirect '/users/home'
